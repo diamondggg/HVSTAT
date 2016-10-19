3a3f884e54a0a6ce6a2d36c7ebe015db0d97c66e
diamondggg <diamondggg@yandex.ru>
2016-10-20 00:47:19 +0300

    version bump

--- data/hvstat.user.js
+++ data/hvstat.user.js
@@ -9,7 +9,7 @@
 // @exclude         http://alt.hentaiverse.org/pages/showequip*
 // @exclude         http://alt.hentaiverse.org/?login*
 // @author          Various (http://forums.e-hentai.org/index.php?showtopic=79552)
-// @version         5.6.8.4
+// @version         5.6.9
 // @require         scripts/util.js
 // @require         scripts/browser.js
 // @require         scripts/hv.js
@@ -59,7 +59,7 @@ window.IDBCursor = window.IDBCursor || window.webkitIDBCursor;
 // HV STAT object
 //------------------------------------
 var hvStat = {
-	version: "5.6.8.4",
+	version: "5.6.9",
 	imageResources: [
 		new browser.I("images/", "channeling.png", "css/images/"),
 		new browser.I("images/", "healthpot.png", "css/images/"),
